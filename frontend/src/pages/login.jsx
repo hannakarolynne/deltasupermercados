a3d8f0ffbf2643b537f941c0614fc6a56cf56e7f
Davi-Noah <daviollee@gmail.com>
2024-12-14 15:54:13 -0300

    Subindo versão 01

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,50 @@
+import './login.css'
+
+function Login() {
+
+
+  return (
+    <>
+            <div class="login">
+            <img class="image-removebg-preview-1" src="image-removebg-preview-1.png" />
+            <div class="component-3">
+            <div class="login2">
+            <div class="ellipse-1"></div>
+            <img class="ellipse-2" src="ellipse-20.svg" />
+            <div class="ellipse-3"></div>
+            <img class="rectangle-1" src="rectangle-10.svg" />
+            <div class="bem-vindo">BEM VINDO</div>
+            <div class="rectangle-2"></div>
+            <div class="rectangle-3"></div>
+            <img class="rectangle-4" src="rectangle-40.svg" />
+            <div class="rectangle-6"></div>
+            <div class="usu-rio-ou-cpf">USUÁRIO OU CPF</div>
+            <div class="senha">SENHA</div>
+            <div class="cadastrar">CADASTRAR</div>
+            <img class="done" src="done0.png" />
+            <img class="user-male" src="user-male0.png" />
+            <img class="lock" src="" />
+            <div class="lembrar-me">LEMBRAR-ME</div>
+            <div class="component-1">
+                <div class="group-2">
+                <div class="_1234">1234</div>
+                </div>
+                <img class="eye" src="eye0.png" />
+            </div>
+            <img class="rectangle-18" src="rectangle-180.svg" />
+            <div class="login3">Login</div>
+            <div class="temos-o-orgulho-de-receber-voc">
+                Temos o orgulho de receber você!
+            </div>
+            <div class="rectangle-6"></div>
+            <div class="cadastrar">CADASTRAR</div>
+            <div class="rectangle-22"></div>
+            <div class="entrar">Entrar</div>
+            </div>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Login
