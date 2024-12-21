60323e8ee8136cb7364029994ec2bda5c9245afc
Kagu-13 <joselevipazpessoa@gmail.com>
2024-12-21 16:09:04 -0300

    cadastro e login - backend inicial

---
+++ backend/models/userModels.js
@@ -0,0 +1,15 @@
+const pool = require('../config/db.js')
+
+const findUserByEmail = async (email) => {
+    const [rows] = await pool.query('SELECT = FROM users WHERE email = ?', [email])
+    return rows[0]
+}
+
+const createUser = async (name, email, passwordHash) =>{
+    const [result] = await pool.query(
+        'INSERT INTO users (name, email, password) VALUES (?,?,?)', [name, email, passwordHash]
+    )
+    return result.insertId
+}
+
+module.exports = {findUserByEmail, createUser}
