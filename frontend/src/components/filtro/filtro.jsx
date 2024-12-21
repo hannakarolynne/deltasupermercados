321aeda9e08da99ae0d2ae20a05246015ccb8f46
ReprovadyN <gabrielsup@alu.ufc.br>
2024-12-21 15:48:19 -0300

    subindo filto e menu

---
+++ frontend/src/components/filtro/filtro.jsx
@@ -0,0 +1,51 @@
+import './filtro.css'
+
+function Filtro() {
+    return (
+        <>
+            <div class="filtro">
+                <div class="rectangle-27"></div>
+                <div class="roupas">
+                <a href="#">Roupas</a>
+                    <br />
+                </div>
+                <div class="x">X</div>
+                <div class="filtrar-por">Filtrar por:</div>
+                <div class="eletr-nicos">
+                    <a href="#">Eletrônicos</a>
+                    <br />
+                </div>
+                <div class="casa">
+                <a href="#">Casa</a>
+                        <br />
+                </div>
+                <div class="alimentos">
+                <a href="#">Alimentos</a>
+                        <br />
+                </div>
+                <div class="cosm-ticos">
+                <a href="#">Cosméticos</a>
+                </div>
+                <div class="ofertas">
+                <a href="#">Ofertas</a>
+                    <br />
+                </div>
+                <div class="limpeza">
+                <a href="#">Limpeza</a>
+                </div>
+                <div class="pre-o">
+                <a href="#">Preço</a>
+                    <br />
+                </div>
+                <div class="pet">
+                <a href="#">Pet</a>
+                </div>
+                <div class="higiene">
+                <a href="#">Higiene</a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Filtro
---
+++ frontend/src/components/menu/menu.jsx
@@ -0,0 +1,30 @@
+import './menu.css'
+
+function Menu() {
+
+
+    return (
+        <>
+            <div class="menu-catalogo">
+                <div class="rectangle-16"></div>
+                <div class="x">X</div>
+                <div class="acompanhe-seu-pedido">Acompanhe seu pedido</div>
+                <img class="delivery-scooter" src="delivery-scooter0.png" />
+                <div class="cupons">
+                    Cupons
+                    <br />
+                </div>
+                <img class="loyalty-card" src="loyalty-card0.png" />
+                <div class="vale-presentes">Vale-presentes</div>
+                <img class="gift-card" src="gift-card0.png" />
+                <div class="afiliados">
+                    Afiliados
+                    <br />
+                </div>
+                <img class="user-account" src="user-account0.png" />
+            </div>
+        </>
+    )
+}
+
+export default Menu
