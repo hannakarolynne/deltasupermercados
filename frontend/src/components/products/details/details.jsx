afac6dd58ca1df70d2b30a0e33709cd2661035a2
Youth_Space_32 <jorgegustavovieira1@gmail.com>
2024-12-21 16:09:03 -0300

    subindo a pasta produtos

---
+++ frontend/src/components/products/details/details.jsx
@@ -0,0 +1,73 @@
+import "./details.css"
+
+function Detalhes() {
+    return (
+        <div class="compra-claro">
+            <img class="image-13" src="image-130.png" />
+            <div class="rectangle-60"></div>
+            <div class="rectangle-65"></div>
+            <div class="comprar">Comprar</div>
+            <div class="avaliar">Avaliar</div>
+            <div class="salgadinho-batata-frita-pringles-churrasco-109-g">
+                Salgadinho Batata Frita
+                <br />
+                Pringles Churrasco 109g
+            </div>
+            <div class="r-15-95">R$ 15,95</div>
+            <div class="rectangle-48"></div>
+            <div class="r-14-35">R$ 14,35</div>
+            <img class="star" src="star0.png" />
+            <img class="star2" src="star1.png" />
+            <img class="star3" src="star2.png" />
+            <img class="star4" src="star3.png" />
+            <img class="star5" src="star4.png" />
+            <div class="rectangle-64"></div>
+            <img class="star6" src="star5.png" />
+            <img class="star7" src="star6.png" />
+            <img class="star8" src="star7.png" />
+            <img class="star9" src="star8.png" />
+            <img class="star10" src="star9.png" />
+            <img class="star11" src="star10.png" />
+            <img class="star12" src="star11.png" />
+            <img class="star13" src="star12.png" />
+            <img class="star14" src="star13.png" />
+            <img class="star15" src="star14.png" />
+            <div class="avalia-es">Avaliações</div>
+            <div class="descri-o">Descrição</div>
+            <div class="em-estoque">Em estoque</div>
+            <div class="coment-rios-1">Comentários: 1</div>
+            <div class="dono-da-loja-comentou">
+                <span>
+                    <span class="dono-da-loja-comentou-span">Dono da loja</span>
+                    <span class="dono-da-loja-comentou-span2">comentou:</span>
+                </span>
+            </div>
+            <div class="amei-comprem-nota-10000">Amei ❤️, comprem. Nota 10000😋.</div>
+            <img class="sei-la-1" src="sei-la-10.png" />
+            <div class="curtidas-1">
+                <span>
+                    <span class="curtidas-1-span">Curtidas:</span>
+                    <span class="curtidas-1-span2">1</span>
+                </span>
+            </div>
+            <img class="heart" src="heart0.png" />
+            <div class="rectangle-35"></div>
+            <div class="rectangle-24"></div>
+            <div class="rectangle-23"></div>
+            <div class="modo-escuro">Modo Escuro</div>
+            <div class="rectangle-36"></div>
+            <img class="image-removebg-preview-2" src="image-removebg-preview-20.png" />
+            <img class="group-23" src="group-230.svg" />
+            <img class="menu" src="menu0.png" />
+            <div class="busque-produtos-aqui">Busque produtos aqui</div>
+            <img class="shopping-cart" src="shopping-cart0.png" />
+            <img class="filter" src="filter0.png" />
+            <div
+                class="home-produto-cat-logo-salgadinho-batata-frita-pringles-churrasco-109-g"
+            >
+                Home &gt; Produto &gt; Catálogo &gt; Salgadinho Batata Frita Pringles
+                Churrasco 109g
+            </div>
+        </div>
+    )
+}export default Detalhes
