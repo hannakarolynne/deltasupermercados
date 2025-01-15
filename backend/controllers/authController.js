c37b0286d2d03ef6e6bb40533e69794ddbb78288
Luana <luanaforte950@gmail.com>
2025-01-15 18:10:39 -0300

    subindo token de autenticação

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,7 @@
-const  bcrypt = require('bcrypt')
-const twt = require ('jsaonwebtoken')
+const  bcrypt = require ('bcrypt')
+
+const jwt = require ('jsaonwebtoken')
+
 const userModel = require('../models/userModels.js')
 
 const register = async (req,res) =>{
@@ -12,25 +14,24 @@ const register = async (req,res) =>{
 
         res.status(201).json({message: 'Usuário criado', userId})
 
-    } catch (error){
-res.status(500).json({error: error.message})
-}
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
 }
 
 const login = async (req, res) => {
     const {email, password} = req.body
+
     try{
         const user = await userModel.findUserByEmail(email)
         if (!user) return res.status(400).json({message: 'Usuário ou senha inválidos'})
-
-
 
         const isPasswordvalid = await bcrypt.compare(password, user.password)
         if (!isPasswordvalid) return res.status(400).json ({message: 'Usuário ou senha inválidos'})
 
         const token = jwt.sign({id:user.id}, process.env.JWT, {expiresIn: '1h'})
-    console.log(token)
-    res.json({token})
+        console.log(token)
+        res.json({token})
 
     }
     catch {
