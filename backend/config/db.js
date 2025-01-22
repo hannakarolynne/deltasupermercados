a6d40a6b744256c957ebd497ca67c2fdbc12799b
dionisio300 <dionisioassis80@gmail.com>
2025-01-22 01:34:12 -0300

    Conexão com o banco de dados

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,22 @@
 const mysql = require('mysql2/promise')
-require('dotenv').config()
+require('dotenv').config({path: '../.env'})
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 })
+
+// const testConnection = async () => {
+//     try {
+//         const [rows] = await pool.query('SELECT 1')
+//         console.log('Conexão com o banco de dados bem-sucedida!')
+//     } catch (error) {
+//         console.error('Erro ao conecta com o banco de dados:', error.message)
+//     }
+// }
+
+// testConnection()
 
 module.exports = pool
